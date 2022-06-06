f60bf4a2334a57ad908bef57d44d0213691e5a67
fuhaiwei <fuhaiwei@126.com>
2022-06-06 20:03:19 +0800

    Format rabbitmq/app.sh

--- rabbitmq/app.sh
+++ rabbitmq/app.sh
@@ -2,24 +2,24 @@ name="app-rabbitmq"
 dest="rabbitmq:3-management"
 home="$(realpath $(dirname $0))"
 
-mkdir -p "$home"/data
+mkdir -p $home/data
 
-case "$1" in
+case $1 in
 init)
-    sudo docker rm -f "$name"
-    sudo docker run --name "$name" \
-        --hostname "$name" \
-        -v "$home"/data:/var/lib/rabbitmq \
-        -v "$home"/etc:/etc/rabbitmq:ro \
+    sudo docker rm -f $name
+    sudo docker run --name $name \
+        --hostname $name \
+        -v $home/data:/var/lib/rabbitmq \
+        -v $home/etc:/etc/rabbitmq:ro \
         -p 5672:5672 \
         -p 15672:15672 \
-        -d "$dest"
+        -d $dest
     ;;
 stop)
-    sudo docker stop "$name"
+    sudo docker stop $name
     ;;
 bash)
-    sudo docker exec -it "$name" bash
+    sudo docker exec -it $name bash
     ;;
 *)
     echo "sh app.sh init"

d914f04ca68e35b6982974bb39bbfca7f9f60598
fuhaiwei <fuhaiwei@126.com>
2022-06-08 12:43:59 +0800

    Update docker nginx

--- nginx/app.sh
+++ nginx/app.sh
@@ -3,8 +3,8 @@ dest="nginx:mainline"
 home="$(realpath $(dirname $0))"
 
 mkdir -p $home/etc/cert.d
-mkdir -p $home/log
 mkdir -p $home/www
+mkdir -p $home/log
 
 case $1 in
 init)
@@ -13,8 +13,8 @@ init)
         -v $home/etc/nginx.conf:/etc/nginx/nginx.conf:ro \
         -v $home/etc/conf.d:/etc/nginx/conf.d:ro \
         -v $home/etc/cert.d:/etc/nginx/cert.d:ro \
-        -v $home/log:/var/log/nginx \
         -v $home/www:/usr/share/nginx/html:ro \
+        -v $home/log:/var/log/nginx \
         -p 80:80 \
         -p 81:81 \
         -p 443:443 \

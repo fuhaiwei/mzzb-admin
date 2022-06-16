1ba863807682336ad3542f0deb9a8469c0542993
fuhaiwei <fuhaiwei@126.com>
2022-06-16 09:03:12 +0800

    Update system/do_apt_upgrade.sh

--- system/do_apt_upgrade.sh
+++ system/do_apt_upgrade.sh
@@ -22,7 +22,7 @@ Log "== Do Apt Upgrade: $(Now) ==" >>$logfile
 
 Run sudo apt-get update
 
-Run sudo apt list --upgradable 2>/dev/null
+Run sudo apt list --upgradable
 
 Run sudo apt-get dist-upgrade -y
 
